8e85ab8dcd2a51649ace3e07ecb6b9c2d168fa6e
Daniel Ditgens <daniel@ditgens.net>
2015-07-22 23:41:41 +0200

    Personalized error

--- gulp/util/errors.js
+++ gulp/util/errors.js
@@ -3,7 +3,7 @@ var notify = require("gulp-notify");
 module.exports = function() {
   var args = Array.prototype.slice.call(arguments);
   notify.onError({
-    title: "Error",
+    title: "Whiplash: Error",
     message: "<%= error %>"
   }).apply(this, args);
   this.emit('end');
