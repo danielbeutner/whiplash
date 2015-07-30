de3972ae9b5644ce4fe1bc67a3636e6a61ba3b03
Daniel Ditgens <daniel@ditgens.net>
2015-07-30 17:19:11 +0200

    Added jslint and 'use strict'

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
+/*jslint node: true */
+'use strict';
+
 var gulp = require('gulp'),
     requireDir = require('require-dir');
 
 requireDir('./gulp/tasks', { recurse: true });
 
-gulp.task('default', ['build'], function() {
+gulp.task('default', ['build'], function () {
   gulp.start('watch');
 });
