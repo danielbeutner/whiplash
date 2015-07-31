b402dad4b64aea01167611bc3e66da92a9ec6200
Daniel Ditgens <daniel@ditgens.net>
2015-07-31 12:53:20 +0200

    Added support for mustache in html

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -3,10 +3,17 @@
 
 var gulp = require('gulp'),
     sync = require('browser-sync'),
+    mustache = require('gulp-mustache'),
+    rename = require("gulp-rename"),
+    debug = require('../config').debug,
     config = require('../config').html;
 
 gulp.task('html', function() {
   gulp.src(config.src)
+    .pipe(mustache({
+      debug: debug
+    }))
+    .pipe(rename({extname: '.html' }))
     .pipe(gulp.dest(config.dest))
     .pipe(sync.stream());
 });
